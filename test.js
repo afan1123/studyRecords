430caf4eafeec82d5e3d6d21b3f1fe2e77755eb9
zhangyufan <zhangyufan@comisys.net>
2022-12-13 14:00:58 +0800

    feat: 12-13

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
-const obj = {};
-Object.defineProperty(obj, 'name', {});
-obj.name = 1;
-console.log(obj.name); //undefined
-console.log(Object.getOwnPropertyDescriptor(obj, 'name'));
+document.getElementById('btn').addEventListener('click', updateStyleNode);
+function updateStyleNode() {
+  const styleSheets = Array.from(document.styleSheets);
+  // onwerNode获得styleSheet对应的节点
+  const st = styleSheets.find((s) => s.onwerNode.id == 'ss-test');
+  // 通过选择器找到对应的rule
+  const rule = Array.form(st.rules).find((r) => r.selectorText === '.div');
+}
+const styleMap = rule.styleMap;
+styleMap.set('background-color', 'yellow');
